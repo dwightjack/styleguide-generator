98bfc4580074cd89a050ad13054b90ed2b42c0ab
Marco Solazzi <marco.solazzi@gmail.com>
2018-02-06 13:22:53 +0900

    generate by default and return a promise

--- tests/fixtures/demo.js
+++ tests/fixtures/demo.js
@@ -1,17 +1,5 @@
 const { generate } = require('../../lib/generator');
 
-const generator = generate();
-
-generator.fromSources(['src', 'docs'], true, (err, files) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log('Done!');
-    console.log(
-        `The following files have been generated in ${generator.destination()}:`
-    );
-    Object.keys(files).forEach((filename) => {
-        console.log(`  * ${filename}`);
-    });
+generate({
+    src: ['src', 'docs']
 });
