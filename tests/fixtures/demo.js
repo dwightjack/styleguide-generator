cf86fb22ebff781e2e6f92adbff335d31e14e157
Marco Solazzi <marco.solazzi@gmail.com>
2018-02-08 11:58:36 +0900

    support media files

--- tests/fixtures/demo.js
+++ tests/fixtures/demo.js
@@ -3,9 +3,5 @@ const { generate } = require('../../lib/generator');
 generate({
     src: ['src', 'docs'],
     dest: 'public/styleguide',
-    baseUrl: '/styleguide',
-    watch: true,
-    server: {
-        baseDir: ['public']
-    }
+    baseUrl: '/styleguide'
 });
